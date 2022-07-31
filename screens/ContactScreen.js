593535d53a30531689221b0905ab8e47864873aa
Dilan Udawattha <87587934+Dil2010@users.noreply.github.com>
2022-07-30 20:01:32 -0700

    Updates

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,6 +1,6 @@
 import { ScrollView, Text } from 'react-native';
 import { Card } from 'react-native-elements';
-​
+
 const ContactScreen = () => {
     return (
         <ScrollView>
@@ -32,5 +32,42 @@ const ContactScreen = () => {
         </ScrollView>
     );
 };
-​
-export default ContactScreen;
+
+export default ContactScreen; 
+
+// import { ScrollView, Text } from 'react-native';
+// import { Card } from 'react-native-elements';
+
+// const ContactScreen = () => {
+//     return (
+//         <ScrollView>
+//             <Card wrapperStyle={{margin: 10}}>
+//                 <Card.Title>
+//                     Contact Information
+//                 </Card.Title>
+//                 <Card.Divider/>
+//                 <Text>
+//                     1 nucamp Way
+//                 </Text>
+//                 <Text>
+//                     Seattle, WA 90001
+//                 </Text>
+//                 <Text 
+//                     style={{
+//                     marginBottom: 10
+//                 }}
+//                 >
+//                     U.S.A
+//                 </Text>
+//                 <Text>
+//                     Phone: 1-206-555-1234
+//                 </Text>
+//                 <Text>
+//                     Email: campsites@nucamp.co
+//                 </Text>
+//             </Card>
+//         </ScrollView>
+//     );
+// };
+
+// export default ContactScreen;
